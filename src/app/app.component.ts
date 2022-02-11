c529ad591b3c09f16d2eee03c2e1601a1a7a2962
Bukingemskiy <vlad-3006@yandex.ru>
2022-02-11 23:48:11 +0300

    fixed images

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,18 +18,18 @@ export class AppComponent {
   cell = true
 
   initialCards = [
-    { title: "Shironeko", description: "Самый счастливый и сонный кот в мире", image: "//html.ftpes.ru/FrontEndTest/Shironeko.jpg" },
-    { title: "Garfi", description: "Самый злой кот в мире", image: "//html.ftpes.ru/FrontEndTest/Garfi.jpg" },
-    { title: "Sam", description: "У кота Сэма есть потрясающие брови", image: "//html.ftpes.ru/FrontEndTest/Sam.jpg" },
-    { title: "Snoopy", description: "Самый очаровательный котик", image: "//html.ftpes.ru/FrontEndTest/Snoopy.jpg" },
-    { title: "Venus", description: "Кошка с двумя лицами", image: "//html.ftpes.ru/FrontEndTest/Venus.jpg" },
-    { title: "Maru", description: "Повелитель коробок", image: "//html.ftpes.ru/FrontEndTest/Maru.jpg" },
-    { title: "Lil Bub", description: "Вечный котёнок", image: "//html.ftpes.ru/FrontEndTest/Lil_Bub.jpg" },
-    { title: "Banye", description: "Всегда удивлённый кот", image: "//html.ftpes.ru/FrontEndTest/Banye.jpg" },
-    { title: "Grumpy Cat", description: "Сердитый котик", image: "//html.ftpes.ru/FrontEndTest/Grumpy_Cat.jpg" },
-    { title: "Hamilton", description: "Кот-хипстер", image: "//html.ftpes.ru/FrontEndTest/Hamilton.jpg" },
-    { title: "Nala", description: "Кошка, которая всегда в шоке", image: "//html.ftpes.ru/FrontEndTest/Nala.jpg" },
-    { title: "Colonel Meow", description: "Кот с самой длинной шерстью", image: "//html.ftpes.ru/FrontEndTest/Colonel_Meow.jpg" }
+    { title: "Shironeko", description: "Самый счастливый и сонный кот в мире", image: "../../assets/Shironeko.jpg" },
+    { title: "Garfi", description: "Самый злой кот в мире", image: "../../assets/Garfi.jpg" },
+    { title: "Sam", description: "У кота Сэма есть потрясающие брови", image: "../../assets/Sam.jpg" },
+    { title: "Snoopy", description: "Самый очаровательный котик", image: "../../assets/Snoopy.jpg" },
+    { title: "Venus", description: "Кошка с двумя лицами", image: "../../assets/Venus.jpg" },
+    { title: "Maru", description: "Повелитель коробок", image: "../../assets/Maru.jpg" },
+    { title: "Lil Bub", description: "Вечный котёнок", image: "../../assets/Lil_Bub.jpg" },
+    { title: "Banye", description: "Всегда удивлённый кот", image: "../../assets/Banye.jpg" },
+    { title: "Grumpy Cat", description: "Сердитый котик", image: "../../assets/Grumpy_Cat.jpg" },
+    { title: "Hamilton", description: "Кот-хипстер", image: "../../assets/Hamilton.jpg" },
+    { title: "Nala", description: "Кошка, которая всегда в шоке", image: "../../assets/Nala.jpg" },
+    { title: "Colonel Meow", description: "Кот с самой длинной шерстью", image: "../../assets/Colonel_Meow.jpg" }
   ]
 
   newCards: Card[] =
